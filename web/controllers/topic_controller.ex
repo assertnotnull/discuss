c5606785b3a979db68aa2e0826b5611b697efc1e
assertnotnull <patgauth@gmail.com>
2019-01-15 23:37:40 -0500

    redirect and flash upon insert

--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -24,8 +24,11 @@ defmodule Discuss.TopicController do
         changeset = Topic.changeset(%Topic{}, topic)
 
         case Repo.insert(changeset) do
-            {:ok, post} -> IO.inspect(post)
-            {:error, changeset} -> 
+            {:ok, post} ->
+                conn
+                |> put_flash(:info, "Topic Created")
+                |> redirect(to: topic_path(conn, :index))
+            {:error, changeset} ->
                 render conn, "new.html", changeset: changeset
         end
     end
